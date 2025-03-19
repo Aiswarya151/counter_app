c8d9ebbd36b02cb2e4fd0049c13121b5f8322f78
Aiswarya151 <aiswaryashaji55@gmail.com>
2025-03-19 13:45:16 +0530

    Added theme and routes

---
+++ lib/core/router/router.dart
@@ -0,0 +1,19 @@
+import 'package:counter_app/features/counter/counter_screen.dart';
+import 'package:counter_app/features/home/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: 'home',
+      builder: (context, state) => HomeScreen(),
+    ), // GoRoute
+    GoRoute(
+      path: '/counter',
+      name: 'counter',
+      builder: (context, state) => CounterScreen(),
+    ), // GoRoute
+  ],
+); // GoRouter
---
+++ lib/features/home/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: ElevatedButton(onPressed: () {context.pushNamed('counter');}, child: Text('Press to new page'))),
+    );
+  }
+}
